e5401eefe182190c039d50804c4b2de8efb3f3a9
bekirbali <bfbali43@gmail.com>
2023-05-31 21:42:46 +0300

    app store provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import FavoritesPage from "./pages/FavoritesPage";
 import NavBar from "./components/NavBar";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
     <div className="bg-slate-700 min-h-screen text-center">
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
