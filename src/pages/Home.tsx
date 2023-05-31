c4e3be034e8a449d65bc171e86e0918cdfcc272b
bekirbali <bfbali43@gmail.com>
2023-05-31 22:12:48 +0300

    search component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchComp from "../components/SearchComp";
+import axios from "axios";
+import { Product, fetchStart } from "../features/productsSlice";
+import { useAppDispatch } from "../app/hooks";
+
+export interface Products {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
 
 const Home = () => {
-  return <div>Home</div>;
+  const [search, setSearch] = useState<string>("");
+  const dispatch = useAppDispatch();
+
+  const getData = async () => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.get<Products>(
+        `https://dummyjson.com/products/search/?${search}`
+      );
+      console.log(data.limit);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <SearchComp />
+    </div>
+  );
 };
 
 export default Home;
